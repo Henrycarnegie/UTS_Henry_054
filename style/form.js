cea8d421c8013588391421aa940e23dd1baa6d99
Henry Carnegie <99797029+Henrycarnegie@users.noreply.github.com>
2023-10-23 18:48:46 +0700

    Add files via upload

---
+++ style/form.js
@@ -0,0 +1,12 @@
+    let nama = document.getElementById('nama').value;
+    let nim = document.getElementById('nim').value;
+    
+    // let programStudi = document.getElementById('programStudi').value;
+    
+    // let jenisKelamin = document.querySelector('#jenisKelamin').value;
+    // let tanggal = document.querySelector('tanggalLahir').value;
+
+    // Tampilkan data dalam tabel
+    document.getElementById("nameCell").innerText = name;
+    document.getElementById("ageCell").innerText = age;
+
